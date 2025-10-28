51ec3d3e3448d1a6a1edec3a543adedd43598ced
Douglas-Lps <olv.douglaslopes@gmail.com>
2025-10-27 22:26:50 -0300

    retira as chaves no caminho da importacao dos icones

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { TaskContext } from "../context/TaskContext";
 
-import {iconeAdicionar} from "../icons/add_circle_preto.svg";
-import {iconeListaVazia} from "../icons/lista-vazia-add.svg";
-import {iconeEditar} from "../icons/edit-square.svg";
-import {iconeLixeira} from "../icons/icone-lixeira.svg";
-import {iconeVisualizar} from "../icons/visibility.svg";
+import iconeAdicionar from "../icons/add_circle_preto.svg";
+import iconeListaVazia from "../icons/lista-vazia-add.svg";
+import iconeEditar from "../icons/edit-square.svg";
+import iconeLixeira from "../icons/icone-lixeira.svg";
+import iconeVisualizar from "../icons/visibility.svg";
 
 function Home(){
 
